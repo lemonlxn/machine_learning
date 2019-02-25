94a1d1622d2c54c0c08c624b8259223a2a2286a3
lemonlxn <38677657+lemonlxn@users.noreply.github.com>
2019-02-25 17:50:17 +0800

    增加衡量线性回归算法的指标

--- playML/metrics.py
+++ playML/metrics.py
@@ -5,6 +5,8 @@
 # @Author  : lemon
 
 import numpy as np
+from math import sqrt
+
 
 def accuracy_score(predict_label,y_test_label):
     '''
@@ -16,3 +18,28 @@ def accuracy_score(predict_label,y_test_label):
     return np.sum(predict_label == y_test_label) / len(y_test_label)
 
 
+def mean_squared_error(y_true,y_predict):
+    '''
+
+    :param y_true: 真实数据
+    :param y_predict: 预测数据
+    :return: 返回 MSE (mean squared error)
+    '''
+    assert len(y_true) == len(y_predict),\
+        '真实数据和预测数据数量应一致'
+
+    return np.sum((y_true - y_predict) ** 2) / len(y_true)
+
+
+def root_mean_squared_error(y_true,y_predict):
+    '''
+    计算 y_true,y_predict 之间的 RMSE
+    '''
+    return sqrt(mean_squared_error(y_true,y_predict))
+
+
+def mean_absolute_error(y_true,y_predict):
+    '''
+    计算 y_true,y_predict 之间的 MAE
+    '''
+    return np.sum(np.absolute(y_true - y_predict)) / len(y_true)
