5269f60209b02743979e432cc8d632cf61fca6f8
lemonlxn <38677657+lemonlxn@users.noreply.github.com>
2019-02-26 10:17:57 +0800

    修改小细节

--- playML/kNN.py
+++ playML/kNN.py
@@ -15,7 +15,7 @@ class KNNClassifier():
     KNN 算法
 
     X_train        测试数据集
-    y_train_label  测试数据标签集
+    y_train  测试数据标签集
 
     X_test         训练数据集
     y_test_label   训练数据标签集
@@ -25,22 +25,24 @@ class KNNClassifier():
         assert k>=1,'k 必须是整数，且得大于等于1'
         self.k = k
         self._X_train = None
-        self._y_train_label = None
+        self._y_train = None
 
-    def fit(self,X_train,y_train_label):
+    def fit(self,X_train,y_train):
+        # 将 X_train,y_train 保存
 
         assert self.k <= X_train.shape[0],\
             'k的数量，需要小于等于训练集的样本数量'
-        assert X_train.shape[0] == y_train_label.shape[0],\
+        assert X_train.shape[0] == y_train.shape[0],\
             '训练集的样本数量，必须和训练集标签数量一致'
 
         self._X_train = X_train
-        self._y_train_label = y_train_label
+        self._y_train = y_train
 
         return self
 
+
     def predict(self,X_predict):
-        assert self._X_train is not None and self._y_train_label is not None,\
+        assert self._X_train is not None and self._y_train is not None,\
             '训练集样本，以及标签，不能为空'
         assert X_predict.shape[1] == self._X_train.shape[1],\
             '预测的样本列数，必须要和训练集列数保持一致'
@@ -48,12 +50,13 @@ class KNNClassifier():
         predict_label = [self._predict(x) for x in X_predict]
         return np.array(predict_label)
 
+
     def _predict(self,x):
 
         distinces = [ sqrt(np.sum((i-x) **2))  for i in self._X_train]
         nearest_distance = np.argsort(distinces)
 
-        top_nearest_label = [self._y_train_label[i] for i in nearest_distance[:self.k]]
+        top_nearest_label = [self._y_train[i] for i in nearest_distance[:self.k]]
         label_type  = Counter(top_nearest_label)
 
         predict_label = label_type.most_common(1)[0][0]
@@ -61,12 +64,12 @@ class KNNClassifier():
         return predict_label
 
 
-
     def score(self,X_test,y_test):
 
         y_predict = self.predict(X_test)
 
         return accuracy_score(y_predict,y_test)
+
 
     def __repr__(self):
         return 'k = %d' % self.k
