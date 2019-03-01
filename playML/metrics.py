e7fcfee48137362b0adda1c2f303b2923c9f87ac
lemonlxn <38677657+lemonlxn@users.noreply.github.com>
2019-03-01 23:20:34 +0800

    增加损失函数最值的debug方法

--- playML/metrics.py
+++ playML/metrics.py
@@ -64,3 +64,36 @@ def r2_score(y_true,y_predict):
         '预测数据集，需要和测试数据集数量保持一致'
 
     return 1 - mean_squared_error(y_true,y_predict) / np.var(y_true)
+
+
+
+def dJ_debug(theta,X_b,y,epsilon = 0.01):
+
+    '''
+    :param theta:
+    :param X_b:     np.hstack([np.ones((len(X), 1)), X])
+    :param y:       标签值
+    :param epsilon: theta 间距
+    :return:        返回该梯度上对应的比较精准的 theta 值
+                    不足点：由于每项theta需要计算 2次 样本数，总体运算速度较慢，不过可以作为调试，作为参照。
+    '''
+
+    def J(theta,X_b,y):
+        try:
+            return np.sum((y-X_b.dot(theta)) ** 2) / len(y)
+        except:
+            return float('inf')
+
+    result = np.empty(len(theta))
+
+    for i in range(len(theta)):
+        theta_1 = theta.copy()
+        theta_1[i] += epsilon
+
+        theta_2 = theta.copy()
+        theta_2[i] -= epsilon
+
+        result[i] = (J(theta_1,X_b,y) - J(theta_2,X_b,y)) / 2 * epsilon
+
+    return result
+
