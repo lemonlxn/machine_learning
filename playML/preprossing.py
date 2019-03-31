6bbfaab10a6e1a718a4812d3f7781e6bed97b4a6
lemonlxn <38677657+lemonlxn@users.noreply.github.com>
2019-03-31 17:40:27 +0800

    新增 通过随机森林 填充缺失值

--- playML/preprossing.py
+++ playML/preprossing.py
@@ -10,26 +10,25 @@ import pandas as pd
 
 def fill_missing_rf(X, y, to_fill_col):
     '''
-    通过随机森林方式，填补缺失值
-
     X : 完整的特征
     y : 完整的标签
     to_fill_col : 需要填补缺失值的列名
     '''
 
-
-    from sklearn.ensemble import RandomForestClassifier
+    import pandas as pd
+    from sklearn.ensemble import RandomForestRegressor
 
     df = pd.concat([X.loc[:, X.columns != to_fill_col], pd.DataFrame(y)], axis=1)
     to_fill = X.loc[:, to_fill_col]
 
     y_train = to_fill[to_fill.notnull()]
-    X_train = df.iloc[y_train.index]
+    X_train = df.iloc[y_train.index, :]
 
     y_test = to_fill[to_fill.isnull()]
-    X_test = df.iloc[y_test.index]
+    X_test = df.iloc[y_test.index, :]
 
-    rf_clf = RandomForestClassifier(n_estimators=100).fit(X_train, y_train)
+    rf_clf = RandomForestRegressor(n_estimators=100)
+    rf_clf.fit(X_train, y_train)
     y_predict = rf_clf.predict(X_test)
 
     return y_predict
